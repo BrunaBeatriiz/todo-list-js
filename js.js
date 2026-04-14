c8687eaab82463e96e94eda8f9632702065191b6
Bruna Beatriz <161722157+BrunaBeatriiz@users.noreply.github.com>
2026-04-14 10:38:28 -0300

    feat: refatoracao UI do to-do-list

--- js.js
+++ js.js
@@ -6,28 +6,31 @@ let lista = document.getElementById("lista");
 let listaTarefas = [];
 
 const salvarTarefa = () => {
-    if(tarefa.value.length === 0){
+    if (tarefa.value.length === 0) {
         alert("Digite uma tarefa valida!");
         return;
-        }
+    }
 
-
-
-    listaTarefas.push(
-        {id: Date.now(),
+    const nova = {
+        id: Date.now(),
         tarefa: tarefa.value,
-        completed: false,});
+        completed: false,
+    };
+
+
+    listaTarefas.push(nova);
     localStorage.setItem("listaTarefasSalva", JSON.stringify(listaTarefas));
     tarefa.value = "";
-    exibirTarefa();
+
+    exibirTarefa(nova.id);
 
 }
 
-function exibirTarefa () {
-    lista.innerHTML= "";
+function exibirTarefa() {
+    lista.innerHTML = "";
 
-    listaTarefas= JSON.parse(localStorage.getItem("listaTarefasSalva"));
-    
+    listaTarefas = JSON.parse(localStorage.getItem("listaTarefasSalva"));
+
     listaTarefas.map((tarefa) => {
         let elemento = document.createElement("li");
         let divEsquerda = document.createElement("div");
@@ -35,12 +38,15 @@ function exibirTarefa () {
         let checkedBox = document.createElement("input");
         let button = document.createElement("button");
 
-         // excluir(tarefa.id) está sendo executado na hora
+
+
+        // excluir(tarefa.id) está sendo executado na hora
         // Você precisa passar uma função, não executar:
-         button.addEventListener('click', () => {excluir(tarefa.id)
-        //  if (confirm('Tem certeza que quer excluir essa tarefa?')) {
-        //     excluir(tarefa.id);
-        //   }
+        button.addEventListener('click', () => {
+            excluir(tarefa.id)
+            //  if (confirm('Tem certeza que quer excluir essa tarefa?')) {
+            //     excluir(tarefa.id);
+            //   }
         }
         );
         button.classList = "material-symbols-outlined";
@@ -49,21 +55,22 @@ function exibirTarefa () {
 
         h2.textContent = tarefa.tarefa;
         button.innerHTML = 'delete'
-        
+
         divEsquerda.appendChild(checkedBox);
         divEsquerda.appendChild(h2);
 
         elemento.appendChild(divEsquerda);
         elemento.appendChild(button);
-        
+
 
 
         lista.appendChild(elemento);
 
         checkedBox.addEventListener("change", () => {
             tarefa.completed = checkedBox.checked;
-            h2.style.textDecoration = checkedBox.checked? h2.classList.add("tarefaConcluida") : h2.classList.remove("tarefaConcluida");
-            
+
+            h2.style.textDecoration = checkedBox.checked ? h2.classList.add("tarefaConcluida") : h2.classList.remove("tarefaConcluida");
+
             localStorage.setItem("listaTarefasSalva", JSON.stringify(listaTarefas));
         })
     })
@@ -71,15 +78,19 @@ function exibirTarefa () {
 
 const excluir = (tarefaId) => {
     const lista = JSON.parse(localStorage.getItem("listaTarefasSalva"));
+
     const listaLimpa = lista.filter((tarefa) => tarefa.id !== tarefaId);
+
     localStorage.setItem("listaTarefasSalva", JSON.stringify(listaLimpa));
     exibirTarefa();
 }
 
 
-function limparTarefas () {
-    listaTarefas= [];
+function limparTarefas() {
+    listaTarefas = [];
+
     lista.textContent = "";
+    
     localStorage.removeItem("listaTarefasSalva")
 }
 
